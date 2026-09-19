3825e123b7da7980e26d5b944a93748253ee74b3
Naleemar <block.boris@mail.ru>
2026-09-19 17:23:44 +0700

    Каркас проекта: .gitignore, заготовка main.cpp, README

---
+++ Maistrenko.cpp
@@ -0,0 +1,11 @@
+#include "ivanov.h"
+#include <cmath>
+// Конечная скорость: v = v0 + a * t
+double finalSpeed(double v0, double a, double t) {
+	return v0 + a * t;
+}
+
+// Перемещение: s = v0 * t + (a * t^2) / 2
+double displacement(double v0, double a, double t) {
+	return v0 * t + (a * t * t) / 2.0;
+}
